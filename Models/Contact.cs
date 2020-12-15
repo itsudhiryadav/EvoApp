0abd0d3e40daf6cfb7b336bef5e2d0d22dffd754
sudhir yadav <it.sudhiryadav@gmail.com>
2020-12-15 19:32:18 +0530

    Added Project

---
+++ Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EvolentDemo.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
---
+++ Models/Data/EvolentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EvolentDemo.Models;
+
+namespace EvolentDemo.Models.Data
+{
+    public class EvolentContext : DbContext
+    {
+        public EvolentContext(DbContextOptions<EvolentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contact { get; set; }
+    }
+}
